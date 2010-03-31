426ad188101424bab80e5fe4d644b51c894741bc
jolleon <jolleon@2fff3ac0-b463-7f91-7641-bb00065bf93e>
2010-03-31 09:36:56 +0000

    view truc

---
+++ trunk/viewcontactdialog.h
@@ -0,0 +1,45 @@
+/****************************************************************************
+**
+** Copyright (C) 2009 by user *
+** user@greenphonesdk *
+**
+** Contact: Qt Extended Information (info@qtextended.org)
+**
+** This file may be used under the terms of the GNU General Public License
+** version 2.0 as published by the Free Software Foundation and appearing
+** in the file LICENSE.GPL included in the packaging of this file.
+**
+** Please review the following information to ensure GNU General Public
+** Licensing requirements will be met:
+**     http://www.fsf.org/licensing/licenses/info/GPLv2.html.
+**
+****************************************************************************/
+
+#ifndef VIEWCONTACTDIALOG_H
+#define VIEWCONTACTDIALOG_H
+
+#include <QtGui>
+#include <qtopiaapplication.h>
+
+#include "neophonebookentry.h"
+#include "ui_viewcontactform.h"
+
+class ViewContactDialog : public QWidget, public Ui_ViewContactDialog
+{
+    Q_OBJECT
+
+    public:
+        ViewContactDialog(NeoPhoneBookEntry* entry, QWidget *parent=0, Qt::WFlags f = 0 );
+        ~ViewContactDialog();
+
+    private slots:
+	void on_editButton_clicked();
+	void on_deleteButton_clicked();
+	void on_callButton_clicked();
+
+    private:
+	NeoPhoneBookEntry * entry;
+};
+
+#endif
+
