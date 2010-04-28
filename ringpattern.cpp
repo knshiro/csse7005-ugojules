02de219f240878bda0b7e3fe53bfd2004804dd91
jolleon <jolleon@2fff3ac0-b463-7f91-7641-bb00065bf93e>
2010-04-28 09:00:13 +0000

    fini mais pas teste de compiler...

--- ringpattern.cpp
+++ ringpattern.cpp
@@ -99,26 +99,38 @@ void RingPattern::playPattern(){
 	// read space variables
 	QValueSpaceItem *ledsP = new QValueSpaceItem("/CSSE4003/LedPattern");
 	QValueSpaceItem *vibP = new QValueSpaceItem("/CSSE4003/VibPattern");
-	vibPatterns[vibP->value()]
 
 	// if led pattern is "Random", generate a random pattern
 	if(ledsP->value().equals("Random")) {
+		blueLed = generateRandomPattern();
+		orangeLed = generateRandomPattern();
+		redLed = generateRandomPattern();
+	}
+	else {
+		// split the leds global pattern into 3 patterns (1 for each led)
+		QList<int> l = ledPatterns[ledsP->value()];
+		for(i=0;i<l.size();i+=5){
+			blueLed.append(l[i]*l[i+3]); blueLed.append(l[i+4]);
+			orangeLed.append(l[i+1]*l[i+3]); orangeLed.append(l[i+4]);
+			redLed.append(l[i+2]*l[i+3]); redLed.append(l[i+4]);
+		}
+	}
 
-	// split the leds global pattern into 3 patterns (1 for each led)
-	QList<int> l = ledPatterns[ledsP->value()];
-	for(i=0;i<l.size();i+=5){
-		blueLed.append(l[i]*l[i+3]); blueLed.append(l[i+4]);
-		orangeLed.append(l[i+1]*l[i+3]); orangeLed.append(l[i+4]);
-		redLed.append(l[i+2]*l[i+3]); redLed.append(l[i+4]);
+	// if vib pattern is "Random", generate a random one
+	if(vibP->value().equals("Random")) {
+		vib = generateRandomPattern();
+	}
+	else {
+		vib = vibPatterns[vibP->value()];
 	}
 
 	// set patterns in threads
 	blueThread.setPattern(blueLed);
 	orangeThread.setPattern(orangeLed);
 	redThread.setPattern(redLed);
-	vibThread.setPattern();
+	vibThread.setPattern(vib);
 
-	qDebug() << "  starting pattern";
+	qDebug() << "  starting pattern (vib:" << vibP->value() << ", leds:" << ledsP->value() << ")" ;
 
 	// start the threads
 	blueThread.start();
@@ -129,17 +141,32 @@ void RingPattern::playPattern(){
 	qDebug() << "  pattern started";
 }
 
-void RingPattern::generateRandomPattern(){
+QList<int> RingPattern::generateRandomPattern(){
+	QList<int> l;
+	int strength, duration, delay;
+	for(int i =0;i<10;i++){
+		strength = 100+(qrand()*155)/RAND_MAX;
+		duration = 50+(qrand()*1950)/RAND_MAX;
+		delay = 50+(qrand()*450)/RAND_MAX;
+		l << strength << duration << 0 << delay;
+	}
+	return l;
+}
 
 
 void RingPattern::startVibrate(){
-	qDebug() << "startVibrate";
+	qDebug() << ">>>> startVibrate";
 	playPattern();
+	qDebug() << "<<<< startVibrate";
 }
 
 void RingPattern::stopVibrate(){
-	qDebug() << "stopVibrate";
-	QVibrateAccessoryProvider::setVibrateNow(false);
+	qDebug() << ">>>> stopVibrate";
+	blueThread.terminate();
+	orangeThread.terminate();
+	redThread.terminate();
+	vibThread.terminate();
+	qDebug() << "<<<< stopVibrate";
 }
 
 RingPattern::~RingPattern()
