5e9374b2ef87a8cc1b4f02141cce76ce3a8b7152
jolleon <jolleon@2fff3ac0-b463-7f91-7641-bb00065bf93e>
2010-04-28 08:37:07 +0000

    juste une sauvegarde au cas ou, ca compile surement pas

---
+++ vibledthread.cpp
@@ -0,0 +1,52 @@
+/****************************************************************************
+**
+** Copyright (C) 2009 by user *
+** user@greenphonesdk *
+**
+** Contact: Qt Extended Information (info@qtextended.org)
+**
+** This file may be used under the terms of the GNU General Public License
+** version 2.0 as published by the Free Software Foundation and appearing
+** in the file LICENSE.GPL included in the packaging of this file.
+**
+** Please review the following information to ensure GNU General Public
+** Licensing requirements will be met:
+**     http://www.fsf.org/licensing/licenses/info/GPLv2.html.
+**
+****************************************************************************/
+
+#include "vibledthread.h"
+#include <QtGlobal> 
+
+VibLedThread::VibLedThread(QString hardwareFile){
+	this.hardwareFile=hardwareFile;
+}
+
+void VibLedThread::setPattern(QList<int> pattern){
+	this.pattern=pattern;
+}
+
+
+void VibLedThread::VibLedThread::run()
+{
+	step(-2);
+}
+
+void VibLedThread::step(int i){
+	i=i+2;
+	if(i<pattern.size()){
+		QFile brightness( harwareFile + "/brightness");
+		brightness.open(QIODevice::WriteOnly | QIODevice::Text | QIODevice::Truncate);
+		QTextStream out(&brightness);
+		out<<pattern.at(i);
+		brightness.close();
+		QTimer::singleShot(pattern.at(i+1), this, SLOT(step(i)));
+	}
+	else {
+		QFile brightness( harwareFile + "/brightness");
+		brightness.open(QIODevice::WriteOnly | QIODevice::Text | QIODevice::Truncate);
+		QTextStream out(&brightness);
+		out<<"0";
+		brightness.close();
+	}
+}
