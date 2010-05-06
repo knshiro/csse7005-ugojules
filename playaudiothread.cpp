36d8b1ac1146dfcababbbf48db79c964091b847a
jolleon <jolleon@2fff3ac0-b463-7f91-7641-bb00065bf93e>
2010-05-06 08:35:07 +0000

    Implemented playaudio, everything works. Signals not tested yet

--- playaudiothread.cpp
+++ playaudiothread.cpp
@@ -2,11 +2,12 @@
 #include <QDir>
 
 PlayAudioThread::PlayAudioThread(QString filename){
-    QAudioOutput *audio = new QAudioOutput();
-    audio->setFrequency( 11025 );
-    audio->setChannels( 1 );
-    audio->setBitsPerSample( 16 );
-    audio->open( QIODevice::WriteOnly );
+    audioOutput = new QAudioOutput();
+    file = NULL;
+    audioOutput->setFrequency( FREQUENCY );
+    audioOutput->setChannels( CHANNELS );
+    audioOutput->setBitsPerSample( BITSPERSAMPLE );
+    audioOutput->open( QIODevice::WriteOnly );
 
     if(filename != NULL && filename != ""){
         loadFile(filename);
@@ -14,9 +15,11 @@ PlayAudioThread::PlayAudioThread(QString filename){
 }
 
 PlayAudioThread::~PlayAudioThread(){
+    stop();
 }
 
-void PlayAudioThread::loadFile(QString filename){
+bool PlayAudioThread::loadFile(QString filename){
+    qDebug() << ">> Load file" << filename;
 
     if(filename.startsWith("/")){
         file = new QFile(filename);
@@ -24,30 +27,87 @@ void PlayAudioThread::loadFile(QString filename){
     else{
         QDir dir = QDir::home();
         dir.cd("Documents");
-        //qDebug << dir.filePath(filename);
+        qDebug() << dir.filePath(filename);
         file = new QFile(dir.filePath(filename));
     }
+    
     if (!file->open(QIODevice::ReadOnly)){
-        //qWarning() << "Cannot open the file" << file->fileName());
+        qDebug() << "Cannot open the file" << file->fileName();
+        return false;
+    } else {
+        qDebug() << "<< File" << file->fileName() << "loaded";
+        return true;
     }
 }
 
 void PlayAudioThread::run(){
 
+    char buffer[BYTESPERDS];
+    qint64 len;
+    active = true;
+    qDebug() << ">>Audio Thread started";
+    while (active && !file->atEnd()){
+        len = file->read(buffer,BYTESPERDS);
+        audioOutput->write( buffer,  len );
+        emit currentSec(file->pos() / BYTESPERDS);
+    }
+    active = false;
+    qDebug() << "<<Audio thread return";
 }
 
 void PlayAudioThread::stop(){
+    qDebug() << "Stop";
+    active = false;
+    qDebug() << "Start waiting";
+    wait(1000);
+    qDebug() << "Finished waiting";
+}
 
-}
-void PlayAudioThread::setOffset(int _offset){
+bool PlayAudioThread::setOffset(int _offset){
+    qint64 bytesOffset = 0;
+    stop();
+
+    bytesOffset = _offset * (BYTESPERDS);
+    if(file != NULL){
+        return file->seek(bytesOffset);
+    }
+    else {
+        return false;
+    }
 }
 
-void PlayAudioThread::play(int offset){
+bool PlayAudioThread::play(int offset){
+    qDebug() << ">>Play";
+    stop();
+    qDebug() << "Audio Thread stopped";
+    if (file != NULL){
+        if (offset >=0){
+            if (setOffset(offset)){
+                qDebug() << "Offset :" << offset << "sec";
+            }
+        }
+        start();
+        return true;
+    }
+    else {
+        qDebug() << "File is NULL !!!";
+        return false;
+    }
 }
+
 void PlayAudioThread::pauseResume(){
 }
-int PlayAudioThread::getDuration(){
 
-    return 0;
+int PlayAudioThread::getDuration(){
+    qint64 fileSize = 0;
+    qint64 duration = 0;
+    if(file != NULL) {
+        qDebug() << "Frequence sur 10" << FREQUENCY/10.0 ;
+        qDebug() << "(FREQUENCY/10.0) * CHANNELS * BITSPERSAMPLE/8" << (FREQUENCY/10.0) * CHANNELS * BITSPERSAMPLE/8;
+        qDebug() << "File size" << file->size() ;
+        fileSize = file->size()/BYTESPERDS;
+    }
+    duration = fileSize;
+    return fileSize;
 }
 
