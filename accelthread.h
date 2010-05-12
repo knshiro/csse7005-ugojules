b26df61dc0127be3e497e0cdfec6658962ec84bd
jolleon <jolleon@2fff3ac0-b463-7f91-7641-bb00065bf93e>
2010-05-12 10:23:08 +0000

    Added class to read the state of the accelerometer

---
+++ accelthread.h
@@ -0,0 +1,54 @@
+#ifndef ACCELTHREAD_H
+#define ACCELTHREAD_H
+
+#include <QThread>
+#include <QFile>
+#include <QtGlobal>
+#include <qtopiaapplication.h>
+#include <QString>
+
+#include <stdio.h>
+#include <time.h>
+#include <sys/times.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdint.h>
+
+/**
+*   @author Ugo Bataillard
+*   @version 1.0
+*   This class is used to play audio files and implements the playback with multithreading.
+*/
+class AccelThread : public QThread
+{
+    Q_OBJECT
+    
+    public :
+       //static enum Side { UP, DOWN };
+
+    public:
+        AccelThread();
+        ~AccelThread();
+        
+        virtual void run();
+        virtual void stop();
+
+        //Side getSide();
+        bool isFacingUp();
+
+    signals:
+        // currentSide(Side side); 
+        void facingUp(bool);
+
+    private:
+        int blockingRead(QFile * file, char * buffer, int count);
+        bool side; //True is UP, False is DOWN
+        bool active;
+        QFile * file;
+
+};
+
+
+
+#endif
