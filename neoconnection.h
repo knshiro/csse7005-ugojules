854085de7f57acfe1d61aaee478f15242ba52982
knshiro <knshiro@2fff3ac0-b463-7f91-7641-bb00065bf93e>
2010-06-01 22:48:27 +0000

    Added new files and modified qbuild.pro for assignment5

---
+++ neoconnection.h
@@ -0,0 +1,54 @@
+/****************************************************************************
+**
+** This file is part of the Qt Extended Opensource Package.
+**
+** Copyright (C) 2008 Trolltech ASA.
+**
+** Contact: Qt Extended Information (info@qtextended.org)
+**
+** This file may be used under the terms of the GNU General Public License
+** version 2.0 as published by the Free Software Foundation and appearing
+** in the file LICENSE.GPL included in the packaging of this file.
+**
+** Please review the following information to ensure GNU General Public
+** Licensing requirements will be met:
+**     http://www.fsf.org/licensing/licenses/info/GPLv2.html.
+**
+****************************************************************************/
+
+#ifndef NEOCONNECTION_H
+#define NEOCONNECTION_H
+
+#include <QTimer>
+#include <qtopia/qtopiaapplication.h>
+
+#include "protocols.h"
+#include "ui_neoconnection.h"
+
+class QWaitWidget;
+class QAction;
+class QBluetoothRfcommSocket;
+class QTextEdit;
+class QLineEdit;
+
+class NeoConnection : public QWidget, public Ui_NeoConnection
+{
+    Q_OBJECT
+    
+    public:
+        NeoConnection( QWidget *parent = 0, Qt::WFlags f = 0 );
+        ~NeoConnection();
+    
+    public slots:
+        void connectSocket();
+        void updateFSM(const QString &command);
+        void on_serverButton_clicked();
+
+    private slots:
+
+    private:
+        int neo_state;
+};
+
+#endif
+
